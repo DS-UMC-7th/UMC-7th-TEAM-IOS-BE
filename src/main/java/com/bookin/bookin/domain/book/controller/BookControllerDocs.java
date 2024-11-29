77ee3f91f875517d202cb7ba72219ddcb5737acb
20210699 <20210699@duksung.ac.kr>
2024-11-29 18:47:56 +0900

    ♻️ Refactor: api 설명 추가

--- src/main/java/com/bookin/bookin/domain/book/controller/BookControllerDocs.java
+++ src/main/java/com/bookin/bookin/domain/book/controller/BookControllerDocs.java
@@ -2,11 +2,15 @@ package com.bookin.bookin.domain.book.controller;
 
 import com.bookin.bookin.domain.book.dto.response.BookListResponse;
 import com.bookin.bookin.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name = "책 관리", description = "책 관리 API")
 public interface BookControllerDocs {
+    @Operation(summary = "책 목록 조회 API", description = "설정한 정렬에 따라 책 목록을 조회합니다.")
     public ApiResponse<BookListResponse> getBooks(
             @RequestParam(value = "sortedBy", required = false) @Parameter(description = "정렬순",
                     examples =
