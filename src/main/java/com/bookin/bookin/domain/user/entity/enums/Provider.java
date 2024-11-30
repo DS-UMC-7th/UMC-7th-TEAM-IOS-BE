bab61f40925ff85d096f2d6198251e470fbd22c8
alschlee <1daysixmeal@gmail.com>
2024-11-30 22:46:53 +0900

    ✨ Feat: 회원가입 API 구현 (#9)

--- src/main/java/com/bookin/bookin/domain/user/entity/enums/Provider.java
+++ src/main/java/com/bookin/bookin/domain/user/entity/enums/Provider.java
@@ -1,11 +1,12 @@
 package com.bookin.bookin.domain.user.entity.enums;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+@Getter
+@RequiredArgsConstructor
 public enum Provider {
-    KAKAO("카카오 로그인"),
-    BOOKIN("자체 로그인");
+    BOOKIN,
+    KAKAO;
 
-    private final String provider;
 }
